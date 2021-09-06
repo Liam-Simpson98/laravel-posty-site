f8333841255e06be49fc349c6fccc9eb31983c49
Liam Simpson <liamrsimpson0319@gmail.com>
2021-09-05 23:40:40 -0600

    Connected database and migrated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="bg-gray-200">
-    <nav class="p-6 bg-white flex justify-between">
+    <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex">
             <li>
                 <a href="" class="p-3">Home</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/register');
+
 Route::get('/posts', function () {
 
     return view('posts.index');
