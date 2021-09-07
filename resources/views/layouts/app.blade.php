cc92386a3fe49ef6d3a64ff1131d7ee42a22d9c2
Liam Simpson <liamrsimpson0319@gmail.com>
2021-09-06 20:06:47 -0600

    Finished authentication aside from middleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,10 @@
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href="" class="p-3">Home</a>
+                <a href="/" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">Dashboard</a>
+                <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="" class="p-3">Post</a>
@@ -27,7 +27,9 @@
                 <a href="" class="p-3">{{ auth()->user()->name }}</a>
             </li>
             <li>
-                <a href="" class="p-3">Logout</a>
+                <form action="{{ route('logout') }}" method="post" class="inline p-3">
+                    <button type="submit">Logout</button>
+                </form>
             </li>
             @endauth
             @guest
